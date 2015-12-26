ce0909a203294dfee5cc3e260ab2422ee97f367d
Gaurav Balyan <gauravbaali@gmail.com>
2015-12-26 19:01:58 +0530

    update

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'gauravbaali@gmail.com'
 license          ''
 description      'Installs/Configures awscloudwatch'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.0.2'
